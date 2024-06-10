9d28ac140ff5e2452fd63ae5179b7db975939c2f
danielMagnetlogistic <daniel.magnet@hotmail.com>
2024-06-10 15:53:29 -0400

    add chartjs

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,17 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
+import {Bar} from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { ref } from 'vue';
+ChartJS.register(Title,Tooltip,Legend,BarElement,CategoryScale,LinearScale)
+const chartData = ref({
+    labels: ['January','February','March'],
+    datasets: [{data: [40,20,12]}]
+})
+const chartOptions = ref({
+    Responsive: true
+})
 </script>
 
 <template>
@@ -14,7 +25,12 @@ import Welcome from '@/Components/Welcome.vue';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+
                 </div>
             </div>
         </div>
